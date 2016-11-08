c9c76731f00e45daec05d8f198f9417361544a59
AnacondaPython <thechinglisher@gmail.com>
2016-11-08 12:21:55 -0500

    how pointers work in C and addresses

---
+++ IntroToPointers.c
@@ -0,0 +1,22 @@
+// http://i.imgur.com/nM4UDA6.png . results
+// http://i.imgur.com/47zkcBI.png . copying this file
+// https://www.youtube.com/watch?list=PL2_aWCzGMAwLZp6LMUKI3cc7pgGsasm2_&v=h-HBipu_1P0 from this video
+
+# include <stdio.h>
+
+int main(void){
+    int a;
+    int *p;
+    p = &a;
+    a =5;
+    
+    
+    
+    //printf("%d",p); //204 syntax error
+    printf("The memory address of a is: %p\n", (void*) &a); //204 
+    printf("The memory address of a is: %p\n", (void*) &p); //64
+    printf("%d\n",*p); //5 dereferencing
+    
+    *p = 8;
+    printf("%d\n", a); //8
+}
